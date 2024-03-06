45e94b282d8d75870904a41d2c639958c286123f
João Paulo <joaopaulomayka@gmail.com>
2024-03-06 19:37:08 -0300

    feat: build the search component
    component responsible for accessing the GeoDB Cities API and requesting the name of the cities together with latitude, longitude and country code

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1f980da3d6msh7db1d5c1e65a6e2p11ab30jsn16a1032de5fd',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
