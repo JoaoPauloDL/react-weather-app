22c1e210947616975e7cd38f75364adb1588208b
João Paulo <joaopaulomayka@gmail.com>
2024-03-08 19:04:13 -0300

    feat: request data from the Weather API to
    fetch data from the weather api about temperature, wind chill, wind and humidity and display it dynamically in the CurrentWeather component

--- src/api.js
+++ src/api.js
@@ -10,4 +10,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "79c9187eaf60008235016632e8f7f0a6";
--- src/components/current-weather/Current-Weather.js
+++ src/components/current-weather/Current-Weather.js
@@ -1,37 +1,34 @@
 import "./Current-Weather.css";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
   return (
     <div className="weather">
       <div className="top">
         <div>
-          <p className="city">Belgrade</p>
-          <p className="weather-description">Sunny</p>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
         </div>
-        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+        <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
       </div>
       <div className="bottom">
-        <p className="temperature">18°C</p>
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
         <div className="details">
           <div className="parameter-row">
             <span className="parameter-label top">Detalhes</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Sensação</span>
-            <span className="parameter-value">22°C</span>
+            <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Vento</span>
-            <span className="parameter-value">2 m/s</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Umidade</span>
-            <span className="parameter-value">54%</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
           </div>
-          <div className="parameter-row">
-            <span className="parameter-label">Pressão</span>
-            <span className="parameter-value">15 hPa</span>
-          </div>
+          
         </div>
       </div>
     </div>
